271bc997c7d1036d639a85853aab4990c41c5e9b
cesantos647 <cesantos647@gmail.com>
2018-08-07 01:35:39 -0700

    Added ending marker

--- Other Projects/Project_1/project1.js
+++ Other Projects/Project_1/project1.js
@@ -1,7 +1,7 @@
 //This is a project that encrypts a message using a caesar cipher
 
 //creates an array for the alphabet and numbers
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 //Takes message and shift number and creates a variable for the encrypted message
