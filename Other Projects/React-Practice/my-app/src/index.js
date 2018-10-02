d02cad03e3c6f6571bcf9d69f2d3a20b5460d1db
cesantos647 <cesantos647@gmail.com>
2018-10-01 23:33:07 -0700

    Removed premade files and tested new files

--- Other Projects/React-Practice/my-app/src/index.js
+++ Other Projects/React-Practice/my-app/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-//import App from './App';
-import Test from './App';
+import Test from './Test';
+import Timer from './Timer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Test name="Joe" />, document.getElementById('root'));
+ReactDOM.render(<div><Timer/><Test name='Joe'/></div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
